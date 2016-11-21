fffaf272d99682678981b3d19271c8d4820bfdb6
simrab <simon.rabate@ipp.eu>
2016-11-21 15:00:06 +0100

    Adapt grade_matching to merge of all libelles before

--- fonction_publique/matching_grade/extract_libelles.py
+++ fonction_publique/matching_grade/extract_libelles.py
@@ -22,37 +22,43 @@ app_name = os.path.splitext(os.path.basename(__file__))[0]
 log = logging.getLogger(app_name)
 
 
-def load_libelles(decennie = None, debug = True, force_recreate = True):
+def load_libelles(decennie = None, debug=False):
     libemploi = get_careers(variable = 'libemploi', decennie = decennie, debug = debug)
     libemploi['libemploi_slugified'] = libemploi.libemploi.apply(slugify, separator = "_")    
+    statut = get_careers(variable = 'statut', decennie = decennie, debug = debug)
+    libemploi = (libemploi.merge(
+            statut.query("statut in ['T', 'H']"),
+            how = 'inner',
+            ))
     libemploi = libemploi[libemploi.libemploi != '']
     return libemploi
 
     
-def main():
-    debug = False
+def main(clean_data=False,debug=False):
     # Etape 1: data_cleaning 
-    raw_data_cleaner.main(
-        raw_directory_path = os.path.join(raw_directory_path, "csv"),
-        clean_directory_path = clean_directory_path,
-        debug = debug,
-        chunksize = None,
-        year_min = 2000,
-        )
+    if clean_data:
+        raw_data_cleaner.main(
+            raw_directory_path = os.path.join(raw_directory_path, "csv"),
+            clean_directory_path = clean_directory_path,
+            debug = debug,
+            chunksize = None,
+            year_min = 2000,
+            )
     # Etape 2: extract_libelles and merge
     decennies = [1950, 1970, 1980, 1990]
     for decennie in decennies:
-       libemploi = load_libelles(decennie=decennie)
+       libemploi = load_libelles(decennie=decennie, debug = debug)
        if decennie == decennies[0]:
            libemploi_all = libemploi
        else: 
            libemploi_all = libemploi_all.append(libemploi)
              
     libemploi_h5 = os.path.join(libelles_emploi_directory,"libemploi.h5")
-    libemplois = libemploi_all.groupby(u'annee')['libemploi_slugified'].value_counts()
+    libemploi_all.rename(columns = dict(statut = 'versant'), inplace = True)
+    libemplois = libemploi_all.groupby([u'annee', u'versant'])['libemploi_slugified'].value_counts()
     log.info("Generating and saving libellés emploi to {}".format(libemploi_h5))
     libemplois.to_hdf(libemploi_h5, 'libemploi')
        
     
 if __name__ == "__main__":
-    sys.exit(main())    
+    sys.exit(main(clean_data=False,debug=True))    
