03cce7aa78770647d03f76b8488f47994c658d8b
Simon Rabaté <s.rabate@pse.local>
2017-03-15 15:57:32 +0100

    wipp

--- fonction_publique/modelisation/select_data.py
+++ fonction_publique/modelisation/select_data.py
@@ -118,36 +118,66 @@ def cleaning_data(generation, sexe, an_aff, libemploi, statut, c_neg, echelon, i
 
 
 def main():
-    list_data = ['1980_1999_carrieres.h5',
-                 '1976_1979_carrieres.h5',
-                 '1970_1975_carrieres.h5',
-                 '1960_1965_carrieres.h5','1966_1969_carrieres.h5',
-                 ]
-    for dataset in list_data:
-        print("Processing data {}".format(dataset))
-           # Fix variables
-        generation =  get_careers(variable = 'generation', data_path = dataset)
-        an_aff =  get_careers(variable = 'an_aff', data_path = dataset)
-        sexe =  get_careers(variable = 'sexe', data_path = dataset)
-        # Carrière
-        libemploi = load_career('libemploi', dataset)
-        c_neg = load_career('c_neg', dataset)
-        statut = load_career('statut', dataset)
-        ib = load_career('ib', dataset)
-        echelon = load_career('echelon', dataset).sort_values(['ident', 'annee','trimestre'])
+    datasets = ['1980_1999_carrieres.h5',]
+#                 '1976_1979_carrieres.h5',
+#                 '1970_1975_carrieres.h5',
+#                 '1960_1965_carrieres.h5',
+#                 '1966_1969_carrieres.h5',
+#                 ]
+#    for dataset in datasets:
+    dataset = '1980_1999_carrieres.h5'
+    print("Processing data {}".format(dataset))
+       # Fix variables
+    variable =  'c_neg'  #'statut', 'libemploi',
+    c_neg =  get_careers(variable = variable, data_path = dataset)
 
-        data_AA = cleaning_data(generation, sexe, an_aff, libemploi, statut, c_neg, echelon, ib, 'AA')
-        data_AT = cleaning_data(generation, sexe, an_aff, libemploi, statut, c_neg, echelon, ib, 'AT')
-        data_ES = cleaning_data(generation, sexe, an_aff, libemploi, statut, c_neg, echelon, ib, 'ES')
+    corps = 'AA'
+    grilles = select_grilles(corps = corps)
+    list_code = list(set(grilles.code_grade_NEG.astype(str)))
+    list_code = ['0' + s for s in list_code]
+    subset_ident = list(set(c_neg.ident[c_neg.c_neg.isin(list_code)]))
+    del c_neg
+    import gc
+    gc.collect()
+    print(subset_ident[:10])
+    permanent_variables = ['generation', 'sexe', 'an_aff']
+    where = "ident in {}".format(subset_ident)  # "[3801063, 2097128, 2097131, 3801072, 2097146, 2097150]"
+    permanent =  get_careers(variables = permanent_variables, data_path = dataset, where = where)
+    print permanent
+    BOUM
 
-        if data == list_data[0]:
-            data_merge_AA = data_AA
-            data_merge_AT = data_AT
-            data_merge_ES = data_ES
-        else:
-            data_merge_AA = data_merge_AA.append(data_AA)
-            data_merge_AT = data_merge_AA.append(data_AT)
-            data_merge_ES = data_merge_AA.append(data_ES)
+    permanent_variables = ['generation', 'sexe', 'an_aff']
+    permanent =  get_careers(variables = permanent_variables, data_path = dataset)
+
+
+    quaterly_variables =  ['ib', 'echelon']
+
+
+
+
+
+
+    an_aff =  get_careers(variable = 'an_aff', data_path = dataset)
+    sexe =  get_careers(variable = 'sexe', data_path = dataset)
+    # Carrière
+    libemploi = load_career('libemploi', dataset)
+    c_neg = load_career('c_neg', dataset)
+    statut = load_career('statut', dataset)
+    ib = load_career('ib', dataset)
+    echelon = load_career('echelon', dataset).sort_values(['ident', 'annee','trimestre'])
+
+    data_AA = cleaning_data(generation, sexe, an_aff, libemploi, statut, c_neg, echelon, ib, 'AA')
+    data_AT = cleaning_data(generation, sexe, an_aff, libemploi, statut, c_neg, echelon, ib, 'AT')
+    data_ES = cleaning_data(generation, sexe, an_aff, libemploi, statut, c_neg, echelon, ib, 'ES')
+
+    if data == list_data[0]:
+        data_merge_AA = data_AA
+        data_merge_AT = data_AT
+        data_merge_ES = data_ES
+    else:
+        data_merge_AA = data_merge_AA.append(data_AA)
+        data_merge_AT = data_merge_AA.append(data_AT)
+        data_merge_ES = data_merge_AA.append(data_ES)
 
     path = os.path.join(save_path, "corpsAA.csv")
     data_merge_AA.to_csv(path)
