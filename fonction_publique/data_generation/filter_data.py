0870a94cad19941446671baa5f93a32cb35ef592
ld <lisa.degalle@gmail.com>
2017-07-24 18:18:38 +0200

    Add add_grilles_variables() to base + setup data_generation folder

--- fonction_publique/data_generation/filter_data.py
+++ fonction_publique/data_generation/filter_data.py
@@ -3,9 +3,11 @@ from __future__ import division
 import numpy as np
 import os
 import pandas as pd
-from fonction_publique.base import grilles
+from fonction_publique.base import grilles, output_directory_path, add_grilles_variable
 
-def read_data(data_path = 'M:/CNRACL/output/select_data', filename = 'corpsAT_1995.csv'):
+
+# Read data and replace cir codes for ATT interns
+def read_data(data_path = os.path.join(output_directory_path, 'select_data'), filename = 'corpsAT_1995.csv'):
     return pd.read_csv(
         os.path.join(data_path, filename),
         index_col = 0,
@@ -124,48 +126,6 @@ def select_no_goings_and_comings_of_rank(data):
     return data.query('ident not in @idents_del')
 
 
-# III. Add echelon variable / Merge with grilles
-def add_echelon_variable(data, grilles = grilles): #FIXME deal with late policy implementation
-    data_after_2011 = data.query('(annee >= 2011)').copy()
-    cas_uniques_with_echelon = list()
-    for annee in range(2011, 2015 + 1):
-        cas_uniques = (data_after_2011
-            .query('annee == @annee')[['c_cir', 'ib']]
-            .drop_duplicates()
-            .set_index(['c_cir', 'ib'])
-            )
-        for (c_cir, ib), row in cas_uniques.iterrows():
-            date_effet_grille = grilles[
-                (grilles['code_grade_NETNEH'] == c_cir) &
-                (grilles['date_effet_grille'] <= pd.datetime(annee, 12, 31))
-                ].date_effet_grille.max()
-            grille_in_effect = grilles[
-                (grilles['code_grade_NETNEH'] == c_cir) &
-                (grilles['date_effet_grille'] == date_effet_grille)
-                ].query('ib == @ib')
-            if grille_in_effect.empty:
-                echelon = -1
-                min_mois = -1
-                moy_mois = -1
-                max_mois = -1
-            else:
-                grille_in_effect = grille_in_effect
-                assert len(grille_in_effect) == 1
-                echelon = grille_in_effect.echelon.values.astype(str)[0]
-                min_mois = grille_in_effect.min_mois.values.astype(int)[0]
-                moy_mois = grille_in_effect.moy_mois.values.astype(int)[0]
-                max_mois = grille_in_effect.max_mois.values.astype(int)[0]
-            cas_uniques_with_echelon.append([annee, c_cir, ib, echelon, min_mois, moy_mois, max_mois])
-    cas_uniques = pd.DataFrame(
-        cas_uniques_with_echelon,
-        columns = ['annee', 'c_cir', 'ib', 'echelon', 'min_mois', 'moy_mois', 'max_mois'],
-        )
-    assert not cas_uniques[['c_cir', 'ib', 'annee']].duplicated().any()
-    data = data.merge(cas_uniques, on = ['c_cir', 'ib', 'annee'], how = 'left')
-    data['echelon'] = data['echelon'].fillna(-2).astype(int)
-    return data
-
-
 # IV. Sample selection based on echelon variable
 def select_non_special_level(data):
     data['echelon'] = data['echelon'].astype(int)
@@ -179,33 +139,24 @@ def select_non_missing_level(data):
     return data.query('ident not in @idents_del').copy()
 
 
-#def select_no_level_jump(data):
-#    data_after_2011 = data.query('(annee >= 2011) & (annee < annee_exit)').copy().sort_values('annee', ascending = True)
-#    assert set(['TTH1', 'TTH2', 'TTH3', 'TTH4']) == set(data_after_2011['c_cir'].unique().tolist())
-#    data_after_2011 = data_after_2011.groupby('ident')['echelon'].apply(set).reset_index()
-#    data_after_2011['minimum'] = data_after_2011['echelon'].apply(lambda x: min(x)).astype(int)
-#    data_after_2011['maximum'] = data_after_2011['echelon'].apply(lambda x: max(x)).astype(int)
-#    data_after_2011['range'] = data_after_2011.apply(lambda x :set(range(x["minimum"], x["maximum"] + 1)), axis=1)
-#    data_after_2011['no_level_jump'] = data_after_2011['echelon'] == data_after_2011['range']
-#    idents_del = data_after_2011.query('no_level_jump == False').ident.unique()
-#    return data.query('ident not in @idents_del').copy()
-
-
 def main():
     data = read_data()
     tracking = []
     tracking.append(['ATT once btw. 11-15', len(data.ident.unique())])
     data2 = replace_interns_cir(data)
+    print "2"
     data3 = select_ATT_in_2011(data2)
     tracking.append(['ATT in 11, interns included', len(data3.ident.unique())])
     data4 = select_next_state_in_fonction_publique(data3)
     tracking.append(['Next state = activity in civil service', len(data4.ident.unique())])
     data5 = select_generation(data4)
     tracking.append(['Generation > 1960', len(data5.ident.unique())])
+    print "5"
     data6 = select_continuous_activity_state(data5)
     tracking.append(['Continuous activity on I', len(data6.ident.unique())])
     data7 = select_positive_ib(data6)
     tracking.append(['IB > 0 on I', len(data7.ident.unique())])
+    print "7"
     data8 = select_non_missing_c_cir(data7)
     tracking.append(['Non missing c_cir on I', len(data8.ident.unique())])
     data9 = select_no_decrease_in_ATT_rank(data8)
@@ -214,17 +165,19 @@ def main():
     tracking.append(['Non decreasing IB on I', len(data10.ident.unique())])
     data11 = select_no_goings_and_comings_of_rank(data10)
     tracking.append(['No goings and comings of rank', len(data11.ident.unique())])
-    data12 = add_echelon_variable(data11, grilles = grilles)
+    print "11"
+    data12 = add_grilles_variable(data11, grilles = grilles, first_year = 2011, last_year = 2015)
     data13 = select_non_special_level(data12)
     tracking.append(['No special levels', len(data13.ident.unique())])
     data14 = select_non_missing_level(data13)
+    print "14"
     tracking.append(['Non missing levels on I', len(data14.ident.unique())])
-#    data15 = select_no_level_jump(data14)
-#    tracking.append(['No echelon jump on min(I), max(I)-1', len(data15.ident.unique())])
     tracking.append(['Definition of I', 'max(an_aff, 2003), min(2015, first year of exit)'])
     tracking = pd.DataFrame(tracking)
     print tracking.to_latex()
+    data14.to_csv(os.path.join('M:/CNRACL/filter', "data_ATT_2011_filtered.csv"))
     return data14
+
 
 if __name__ == "__main__":
     main()
