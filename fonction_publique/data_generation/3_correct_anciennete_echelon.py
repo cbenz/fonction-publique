f9484793822249971c03ab0a0fbfac79cbdd48f7
ld <lisa.degalle@gmail.com>
2017-07-28 16:41:57 +0200

    Save temporary data after time-consuming functions

--- fonction_publique/data_generation/3_correct_anciennete_echelon.py
+++ fonction_publique/data_generation/3_correct_anciennete_echelon.py
@@ -3,13 +3,18 @@ import os
 import pandas as pd
 
 
-data = pd.read_csv(
-    os.path.join('M:/CNRACL/filter', "data_ATT_2011_filtered_after_duration_var_added.csv"),
-    index_col = 0
-    )
 
-data.loc[
-    (data['annee'] == 2011) & (data['min_mois'] != -1) & (data['anciennete_echelon'] >= data['min_mois']),
-    'anciennete_echelon'
-    ] = data['min_mois']
+def fix_anciennete_dans_echelon(data, default_value = None):
+    """"
 
+    """"
+    assert default_value in ['min_mois', 'max_mois']
+    data.loc[
+        (data['annee'] == 2011) & (data['min_mois'] != -1) & (data['anciennete_echelon'] >= data[default_value]),
+        'anciennete_echelon'
+        ] = data[default_value]
+    return data
+
+
+
+def main()
