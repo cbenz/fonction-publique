e51a68bcd630730fa9a65e904df920d9f2d2bf2e
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2017-08-09 11:48:12 +0200

    Fix setup.py

--- setup.py
+++ setup.py
@@ -27,8 +27,8 @@ setup(
         'console_scripts': [
             'grade_matching=fonction_publique.matching_grade.grade_matching:main',
             'grade_matching_from_neg=fonction_publique.matching_grade.grade_matching_from_neg:main',
-            'clean_raw_carreer=fonction_publique.scripts.clean_raw_career:main'
-            'generate_data=fonction_publique.scripts.generate_data:main'
+            'clean_raw_carreer=fonction_publique.scripts.clean_raw_career:main',
+            'generate_data=fonction_publique.scripts.generate_data:main',
             ],
         },
     data_files = [
