c2b5a6bb1cb1b6c8b784ed4cf753edf72ec51644
Simon Rabaté <s.rabate@pse.local>
2017-08-24 10:04:57 +0200

    WIP estimation

---
+++ fonction_publique/estimation/config.R
@@ -0,0 +1,11 @@
+
+##  Paths ####
+rm(list = ls()); gc()
+
+wd =  'U:/Projets/CNRACL/fonction-publique/fonction_publique/estimation/'
+data_path = "M:/CNRACL/output/clean_data_finalisation/"
+save_data_path = "M:/CNRACL/simulation/"
+git_path =  'U:/Projets/CNRACL/fonction-publique/fonction_publique/'
+fig_path = "Q:/CNRACL/Slides/2017_07_CDC/Graphiques/"
+tab_path = "Q:/CNRACL/Slides/2017_07_CDC/Graphiques/"
+grilles_path = "U:/Projets/CNRACL/fonction-publique/fonction_publique/assets/grilles_fonction_publique/"
